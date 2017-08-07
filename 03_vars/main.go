512894990a4b77dd2072f64b4c72152f8b5b7d61
Stephen A. Jarjoura <steve@runester.com>
2017-08-06 23:49:43 -0400

    second day of me running through the lessons

---
+++ 03_vars/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 10
+	b := "Golang"
+	c := 4.17
+	d := true
+
+	fmt.Printf("a := %v (%T)\n", a, a)
+	fmt.Printf("b := %v (%T)\n", b, b)
+	fmt.Printf("c := %v (%T)\n", c, c)
+	fmt.Printf("d := %v (%T)\n", d, d)
+}
---
+++ 04_const/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+const q string = "death & taxes"
+
+func main() {
+	fmt.Println(q)
+}
